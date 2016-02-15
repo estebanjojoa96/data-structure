34471a4080d92859394289c39056b0d888b61ce9
deivyponal <deivyponal@ponal>
2016-02-14 19:38:01 -0500

    STRUCTURES LIST

---
+++ src/unity0/list.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unity0;
+
+/**
+ *
+ * @author deivyponal
+ */
+public class list {
+    
+    private int data [];
+    private int top;
+    
+    public list (){
+        
+        data=new int [100];
+        top= 0;
+        
+    }
+    
+    public void add (int d,int index){
+        
+        top++;
+        
+       for( )
+        
+    }
+    
+}
