b192193a158aaa258af8c201ea50464994cbdd16
AULA1 <AULA1@192.168.31.11>
2016-03-29 18:18:17 -0500

    ImageBrowser Unity1

---
+++ src/Unity1/simplestructures/ImageBrowser/ImageFile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unity1.simplestructures.ImageBrowser;
+
+/**
+ *
+ * @author AULA1
+ */
+public class ImageFile {
+    
+    protected String name;
+    protected ImageFile next,back;
+    
+    public ImageFile(String name){
+        
+        this.name= name;
+        next= null;
+        back=null;
+    
+}
+    
+}
+
+
